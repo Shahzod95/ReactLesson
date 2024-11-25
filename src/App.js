e4bf23a6a167e6e239690140c27ec1114f0c9d3f
Shahzod95 <aktamovsh6975@gmail.com>
2024-11-25 21:56:04 +0500

    lesson life cycle methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { Main as Asosiy } from "./pages/Main"
+import { Component } from "react";
+import Bola from "./Bola";
+// import { Main as Asosiy } from "./pages/Main"
 
 // Reactda komponentlar 2 xil bo'ladi 
 
@@ -6,12 +8,46 @@ import { Main as Asosiy } from "./pages/Main"
 
 // Function component 
 
-function App() {
-  return (
-    <div>
-     <Asosiy />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      toggle: false
+    }
+    console.log("App constructor")
+}
+
+  async componentDidMount(){
+    const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await data.json() 
+    console.log("App mounted... ", users)
+  }
+
+  componentDidUpdate(a, b){
+    console.log("App a = ", a, " APP b = ", b)
+  }
+
+  handleToggle = () => {
+    this.setState((prev) => ({
+      ...prev,
+      toggle: !prev.toggle
+    }))
+  }
+  render(){
+    console.log("APP component")
+    return (
+      <div>
+        <button onClick={this.handleToggle}>Toogle</button>
+        {
+          this.state.toggle && (
+            <Bola title="Bola props" toggle={this.state.toggle} />
+          )
+
+        }
+      </div>
+    );
+
+  }
 }
 
 export default App;
